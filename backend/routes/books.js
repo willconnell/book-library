72c3fe2d0ec8146d01514bc49801af8a32e03fbc
willconnell <willconnell@utexas.edu>
2024-06-24 18:22:38 -0500

    added get endpoint with option for singling out one book by id

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,8 +1,70 @@
 const express = require("express");
 const router = express.Router();
+let books = [
+  {
+    id: 1,
+    title: "All the Pretty Horses",
+    author: "Cormac McCarthy",
+    year_published: 1992,
+    genre: "Western Fiction",
+  },
+  {
+    id: 2,
+    title: "Trust",
+    author: "Hernan Diaz",
+    year_published: 2022,
+    genre: "Historical Fiction",
+  },
+  {
+    id: 3,
+    title: "Flash Boys",
+    author: "Michael Lewis",
+    year_published: 2014,
+    genre: "Non-Fiction",
+  },
+  {
+    id: 4,
+    title: "Tomorrow, and Tomorrow, and Tomorrow",
+    author: "Gabrielle Zevin",
+    year_published: 2022,
+    genre: "Adventure Fiction",
+  },
+  {
+    id: 5,
+    title: "A Walk in the Woods",
+    author: "Bill Bryson",
+    year_published: 1998,
+    genre: "Travel Literature",
+  },
+  {
+    id: 6,
+    title: "Pillars of the Earth",
+    author: "Ken Follett",
+    year_published: 1989,
+    genre: "Historical Fiction",
+  },
+  {
+    id: 7,
+    title: "East of Eden",
+    author: "John Steinbeck",
+    year_published: 1952,
+    genre: "Fiction",
+  },
+];
 
 router.get("/", (req, res) => {
-  res.send("Hello World from books!");
+  const id = req.query.id != undefined ? parseInt(req.query.id) : undefined;
+  if (id === undefined) res.send(books);
+
+  const book = books.find((book) => book.id === id);
+
+  if (!book) {
+    const error = new Error("Book not found");
+    error.status = 404;
+    throw error;
+  }
+
+  res.send(book);
 });
 
 module.exports = router;
