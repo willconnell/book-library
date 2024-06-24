1fad9dcf5c56853da1516825cb2d0a166ea2e488
willconnell <willconnell@utexas.edu>
2024-06-24 18:17:35 -0500

    created books router in node

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const booksRouter = require("./routes/books");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+app.use("/books", booksRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
