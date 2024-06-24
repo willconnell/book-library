17921305fb5fd718cd2c58eaeb0f293418e5eb47
willconnell <willconnell@utexas.edu>
2024-06-24 18:19:52 -0500

    added middleware for parsing bodies && cors rules for our frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 const booksRouter = require("./routes/books");
 
+// Allow requests from http://localhost:3173
+app.use(
+  cors({
+    origin: "http://127.0.0.1:3173",
+    origin: "http://localhost:3173",
+  })
+);
+
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/books", booksRouter);
 
