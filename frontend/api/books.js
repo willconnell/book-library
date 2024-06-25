c22bc39b4a5b56187eddf4637448514e797ca2d7
willconnell <willconnell@utexas.edu>
2024-06-24 19:10:28 -0500

    added rest of the api calls

--- frontend/api/books.js
+++ frontend/api/books.js
@@ -7,4 +7,19 @@ async function getBooks(id) {
   return res.data;
 }
 
-export { getBooks };
+async function createBook(body) {
+  const res = await axios.post(base_url, body);
+  return res.data;
+}
+
+async function updateBook(body) {
+  const res = await axios.put(base_url, body);
+  return res.data;
+}
+
+async function deleteBook(id) {
+  const res = await axios.delete(base_url + `/${id}`);
+  return res.data;
+}
+
+export { getBooks, createBook, updateBook, deleteBook };
