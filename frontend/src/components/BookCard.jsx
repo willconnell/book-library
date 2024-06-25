9041e10f7d439ea4811ff0f110c79e79ea3dcca4
willconnell <willconnell@utexas.edu>
2024-06-24 20:31:29 -0500

    configured delete button

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
+import { deleteBook, getBooks } from "../api/books";
 import styles from "./BookCard.module.css";
 import Button from "./Button";
+import useStore from "../store/store";
 
 function BookCard({ book }) {
+  const { setBooks } = useStore();
+
+  function onDelete() {
+    const shouldDelete = confirm(
+      "Are you sure you want to delete " + book.title + "?"
+    );
+    if (shouldDelete)
+      deleteBook(book.id).then(() => {
+        getBooks().then((books) => setBooks(books));
+      });
+  }
+
   return (
     <div className={styles.bookCard}>
       <div className={styles.bookData}>
@@ -12,7 +26,7 @@ function BookCard({ book }) {
         {book.genre} <br />
       </div>
       <div className={styles.cardButtons}>
-        <Button label="Delete" />
+        <Button label="Delete" onClick={onDelete} />
         <Link to={`/edit/${book.id}`}>
           <Button label="Edit" />
         </Link>
