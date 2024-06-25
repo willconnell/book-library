2d3a27e4725c2b6648f7fee9c08b40b50d232802
willconnell <willconnell@utexas.edu>
2024-06-24 20:11:11 -0500

    added zustand store

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { getBooks } from "../api/books";
+import useStore from "../store/store.js";
 import BookGrid from "../components/BookGrid";
 import Button from "../components/Button";
 import styles from "./Home.module.css";
 
 function Home() {
-  const [books, setBooks] = useState([]);
+  const { books, setBooks } = useStore();
 
   async function fetchBooks() {
     const books = await getBooks();
