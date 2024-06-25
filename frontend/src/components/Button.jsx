049a023c4e2c5af18dffb8b66f6a91917af32f81
willconnell <willconnell@utexas.edu>
2024-06-24 20:08:09 -0500

    added add book button and adjusted button comp styles

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import styles from "./Button.module.css";
 
-export default function Button({ type, label, style, onClick }) {
+export default function Button({ type, label, style, onClick, className }) {
   return (
     <button
-      className={styles.button}
+      className={`${styles.button} ${className}`}
       type={type}
       onClick={onClick}
       style={style}
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import BookGrid from "../components/BookGrid";
 import { getBooks } from "../api/books";
+import BookGrid from "../components/BookGrid";
+import Button from "../components/Button";
+import styles from "./Home.module.css";
 
 function Home() {
   const [books, setBooks] = useState([]);
@@ -17,6 +19,11 @@ function Home() {
   return (
     <>
       <h1>Book Library</h1>
+      <Button
+        label="Add Book"
+        className={styles.addButton}
+        onClick={() => navigate("/create")}
+      />
       <BookGrid books={books}></BookGrid>
     </>
   );
