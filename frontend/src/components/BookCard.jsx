5b1d4d4c586e66730c3d4f278615c45ed8c79501
willconnell <willconnell@utexas.edu>
2024-06-24 19:26:47 -0500

    update root css styles and added styling to bookcard component

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,13 +1,15 @@
+import styles from "./BookCard.module.css";
+
 function BookCard({ book }) {
   return (
-    <div>
-      <div>
+    <div className={styles.bookCard}>
+      <div className={styles.bookData}>
         <h3>{book.title}</h3>
         by {book.author} <br />
         {book.year_published} <br />
         {book.genre} <br />
       </div>
-      <div>
+      <div className={styles.cardButtons}>
         <button>delete</button>
         <button>edit</button>
       </div>
