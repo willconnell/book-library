6f08a97415423342e7969f35111fc21ac1e4187e
William Connell <william.connell@hyliion.com>
2024-06-26 13:06:02 -0500

    added some unit tests for the Home component

--- frontend/src/__tests__/Home.test.jsx
+++ frontend/src/__tests__/Home.test.jsx
@@ -1,14 +1,33 @@
 import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "../routes/Home";
 import { BrowserRouter } from "react-router-dom";
 
-describe("Button", () => {
+describe("Home", () => {
   it("renders without crashing", () => {
     render(
       <BrowserRouter>
         <Home />
       </BrowserRouter>
     );
+  });
+
+  it("contains page title", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByTestId("title").innerHTML).toBe("Book Library");
+  });
+
+  it("contains add book button", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    expect(screen.getByText("Add Book")).toBeInTheDocument();
   });
 });
